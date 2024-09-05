092d1080675ef8f5bc443ad77bc2efad8ad3f370
Shouvik Mohanta <connectshouvik@gmail.com>
2024-09-05 22:17:21 +0530

    feat: optimizations

--- app/(dashboard)/auctions/[id]/bids/page.tsx
+++ app/(dashboard)/auctions/[id]/bids/page.tsx
@@ -1,9 +1,5 @@
-import { Separator } from '@/components/ui/separator';
-import { db } from '@/app/firebase/firebase';
-import { collection, getDoc, doc } from 'firebase/firestore'; // Added 'doc' import
-import { models } from '@/app/firebase/models';
-import { Auction, User } from '../../../../../lib/schema';
 import { Metadata } from 'next';
+import { ComingSoon } from '@/components/page/ComingSoon';
 
 export type AuctionBIDProps = { params: { id: string } };
 export const metadata: Metadata = {
@@ -12,19 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default async function AuctionBIDPage({ params }: AuctionBIDProps) {
-  const user = (
-    await getDoc(doc(collection(db, models.users), params.id))
-  ).data() as Auction;
+  // const user = (
+  //   await getDoc(doc(collection(db, models.users), params.id))
+  // ).data() as Auction;
 
-  return (
-    <div className="space-y-6">
-      <div>
-        <h3 className="text-lg font-medium">Auction BIDs Details</h3>
-        {/* <p className="text-sm text-muted-foreground">
-          Update your user details.
-        </p> */}
-      </div>
-      <Separator />
-    </div>
-  );
+  return <ComingSoon />;
 }
